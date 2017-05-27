7b1f0d2fa3bd023eb6a8969aae77fe541bdb2446
Herb Miller <herb@bobbingwide.com>
2017-05-27 18:03:01 +0100

    Issue #25 Only call bw_googlemap_v3 when lat and long are numeric

--- includes/oik-fields-virtual-google-map.php
+++ includes/oik-fields-virtual-google-map.php
@@ -1,33 +1,33 @@
-<?php // (C) Copyright Bobbing Wide 2015
+<?php // (C) Copyright Bobbing Wide 2015, 2017
 /**
  * Return the Google Map for the given _post_code, _lat and _long
  *
- * Use [bw_show_googlemap] to display a Google Map
- * passing the values of _post_code, _lat and _long 
- * so it doesn't need to get them from oik options
+ * This is similar to using [bw_show_googlemap] to display a Google Map
+ * except we pass the values of _post_code, _lat and _long 
+ * so it doesn't need to get them from oik options.
  *
- * @param array -
+ * @param string $post_code
+ * @param string $lat
+ * @param string $long
+ * @param integer $ID 
  * @return string - Google Map
  */
 function bw_fields_get_google_map( $post_code, $lat=null, $long=null, $ID=null ) {
+	//bw_trace2();
+	//bw_backtrace();
+	
+	if ( is_numeric( $lat )  && is_numeric( $long ) ) {
+		oik_require( "shortcodes/oik-googlemap.php" );
+		$google_map = bw_googlemap_v3( get_the_title( $ID )
+							, $lat
+							, $long
+							, $post_code
+							, "100%"
+							, "400px"
+							);
+	}	else {
+		$google_map = null;
+	}
 
-	bw_trace2();
-	bw_backtrace();
-
-	
-	oik_require( "shortcodes/oik-googlemap.php" );
-	
-
-	
-  $google_map = bw_googlemap_v3( get_the_title( $ID )   
-            , $lat
-            , $long
-            , $post_code
-						, "100%"
-						, "400px"
-            );
-	
-	
-
-  return( $google_map );
+	return( $google_map );
 }
